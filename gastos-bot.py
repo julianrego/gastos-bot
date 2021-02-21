fad20290d6fbe4a1b667663ab68a341fa53de8a5
Julian Rego <julirego_94@hotmail.com>
2021-02-20 22:09:46 -0300

    cambio menor

--- gastos-bot.py
+++ gastos-bot.py
@@ -155,7 +155,7 @@ dispatcher.add_error_handler(error)
 
 updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=__OKEN__)
+                          url_path=__TOKEN__)
 updater.bot.setWebhook('https://gastos-hogar-bot.herokuapp.com/' + __TOKEN__)
 
 updater.idle()
